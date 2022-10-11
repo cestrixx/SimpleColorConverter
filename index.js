ded977ea72038dd41f893a42fa1ae5e85e664796
cestrixx <cestrix@hotmail.com>
2022-10-11 15:15:10 -0300

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
 // run `node index.js` in the terminal
+const simpleColorConverter = require('simple-color-converter');
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+const colorTranslator = (value) => {
+  var color = new simpleColorConverter(value);
+  return color;
+};
+
+// faster
+console.log(
+  'Faster: ',
+  colorTranslator({
+    rgb: { r: 10, g: 200, b: 50 },
+    to: 'cmyk',
+  })
+);
+
+// slower but more convenient
+console.log(
+  'Slower: ',
+  colorTranslator({
+    color: 'rgb 10 200 50',
+    to: 'cmyk',
+  })
+);
+
+console.log(
+  'HEX6(#) => RGB: ',
+  colorTranslator({
+    color: '#99EE99',
+    to: 'rgb',
+  })
+);
+
+console.log(
+  'HEX6(0x) => RGB: ',
+  colorTranslator({
+    color: '0x99EE99',
+    to: 'rgb',
+  })
+);
+
+console.log(
+  'HEX8 => RGBA: ',
+  colorTranslator({
+    color: '99EE9980',
+    to: 'rgba',
+  })
+);
+
+console.log(
+  'Cor => RGBA: ',
+  colorTranslator({
+    color: 'red',
+    to: 'rgba',
+  })
+);
